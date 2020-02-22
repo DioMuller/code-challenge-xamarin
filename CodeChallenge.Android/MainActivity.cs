a2ddfbd3c23203b297689aa91ca2caa77637702a
Diogo Muller <diogo.muller@live.com>
2020-02-22 20:15:34 -0300

    Task #4 - Implement the splash screen done for Android.

--- CodeChallenge.Android/MainActivity.cs
+++ CodeChallenge.Android/MainActivity.cs
@@ -26,7 +26,7 @@ using Android.OS;
 
 namespace CodeChallenge.Droid
 {
-    [Activity(Label = "Code Challenge", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
